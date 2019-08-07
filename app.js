eb755255562ddce32ffd99dfd59c0bace12d4d54
TomLu1323241 <yuming.tom.lu@gmail.com>
2019-08-07 16:15:57 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,76 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAk0uwlPQpSacwZy7Adykq6wsvmN0pMVVk",
+    authDomain: "webapptest-a55d8.firebaseapp.com",
+    databaseURL: "https://webapptest-a55d8.firebaseio.com",
+    projectId: "webapptest-a55d8",
+    storageBucket: "",
+    messagingSenderId: "256940377294",
+    appId: "1:256940377294:web:cd3ef5fde8199de1"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var buttons = document.querySelectorAll("button");
+var input;
+
+for (var i = 0; i < buttons.length; i++) {
+    console.log(buttons[i].innerHTML);
+    AddListener(buttons[i]);
+}
+
+function AddListener (button) {
+    button.addEventListener("click", function() {
+        button.innerHTML = input;
+        updateDatabase();
+        // alert("You have clicked : " + button.value);
+    })
+}
+
+var firestore = firebase.firestore();
+
+const docRef = firestore.collection("Game").doc("1");
+
+function initializeUpdate() {
+    let data = ["#", "#", "#",
+                "#", "#", "#",
+                "#", "#", "#"];
+    docRef.set({
+        Table: data
+    }).then(function() {
+        console.log("Status saved!");
+    }).catch(function(error) {
+        console.log("Got an error: ", error);
+    });
+}
+
+function updateDatabase() {
+    let data = [];
+    for (var i = 0; i < buttons.length; i++) {
+        data.push(buttons[i].innerHTML);
+    }
+    docRef.set({
+        Table: data
+    }).then(function() {
+        console.log("Status saved!");
+    }).catch(function(error) {
+        console.log("Got an error: ", error);
+    });
+}
+
+getRealtimeUpdates = function() {
+    docRef.onSnapshot(function(doc) {
+         if (doc && doc.exists) {
+             const myData = doc.data();
+             for (var i = 0; i < buttons.length; i++) {
+                buttons[i].innerHTML = myData.Table[i];
+            }
+         }
+    });
+}
+
+document.getElementById("input").addEventListener("change", function() {
+    input = document.getElementById("input").value;
+})
+
+initializeUpdate();
+getRealtimeUpdates();
