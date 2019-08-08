5954cae5a2cd08aa660bf5a17085dada26452c46
TomLu1323241 <yuming.tom.lu@gmail.com>
2019-08-08 14:50:38 -0400

    Added Rooms for mutiplayer

--- app.js
+++ app.js
@@ -10,8 +10,39 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-var buttons = document.querySelectorAll("button");
+var firestore = firebase.firestore();
+var docRef;
+
+var buttons = document.getElementsByClassName("button");
+var room = document.getElementById("room");
 var input;
+var result = '';
+
+document.getElementById("host").addEventListener("click", function () {
+    result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < 5; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    firestore.collection("Game").doc(result).set({
+        Table: ["#", "#", "#", "#", "#", "#", "#", "#", "#"]
+    }).then(function () {
+        console.log("Status saved!");
+    }).catch(function (error) {
+        console.log("Got an error: ", error);
+    });
+    docRef = firestore.collection("Game").doc(result);
+    alert(result);
+    input = "X";
+    getRealtimeUpdates();
+});
+
+document.getElementById("join").addEventListener("click", function () {
+    docRef = firestore.collection("Game").doc(document.getElementById("room").value);
+    input = "O";
+    getRealtimeUpdates();
+});
 
 for (var i = 0; i < buttons.length; i++) {
     AddListener(buttons[i]);
@@ -22,23 +53,6 @@ function AddListener(button) {
         button.innerHTML = input;
         updateDatabase();
     })
-}
-
-var firestore = firebase.firestore();
-
-const docRef = firestore.collection("Game").doc("1");
-
-function initializeUpdate() {
-    let data = ["#", "#", "#",
-                "#", "#", "#",
-                "#", "#", "#"];
-    docRef.set({
-        Table: data
-    }).then(function () {
-        console.log("Status saved!");
-    }).catch(function (error) {
-        console.log("Got an error: ", error);
-    });
 }
 
 function updateDatabase() {
@@ -62,13 +76,18 @@ getRealtimeUpdates = function () {
             for (var i = 0; i < buttons.length; i++) {
                 buttons[i].innerHTML = myData.Table[i];
             }
+        } else {
+            alert("Opponent has left the game :P");
         }
     });
 }
 
-document.getElementById("input").addEventListener("change", function () {
-    input = document.getElementById("input").value;
-})
+function deleteGame() {
+    docRef.delete().then(function () {
+        alert("Game endded");
+    }).catch(function (error) {
+        alert("Game endded with error");
+    })
+}
 
-initializeUpdate();
-getRealtimeUpdates();
+window.onbeforeunload = deleteGame;
